1702ac2a4deeb7d286615cd7d7de7ede7a6e8eaf
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-04 18:09:38 +0800

    refactor

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ const key = "API_KEY";
 // console.log(wordList[Math.floor(Math.random() * wordList.length)]);
 
 async function query() {
-  const input = document.querySelector("#input").value;
-
-  let word = input;
-  let url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${key}`;
-  let req = await fetch(url);
-  let res = await req.json();
+  const word = document.querySelector("#input").value;
+  const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${key}`;
+  const req = await fetch(url);
+  const res = await req.json();
 
   let el = "";
-  if (typeof res[0] === 'object' && res.length !== 0) {
-    let defs = res[0].shortdef;
+  // console.log(res[0].shortdef);
 
-    for (let i = 0; i < defs.length; i++) {
-      el += `<li>${defs[i]}</li>`;
+  if (typeof res[0] === "object") {
+    const definitions = res[0].shortdef;
+    const definitionsSize = definitions.length;
+
+    for (let i = 0; i < definitionsSize; i++) {
+      el += `<li>${definitions[i]}</li>`;
     }
   } else {
     el = "<li>No definition available.</li>";
